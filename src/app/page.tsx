fe8333eb54f7266549f5af25ec2bd14ee7fa67cc
John P <johnrprutton@gmail.com>
2025-01-30 22:46:26 +0200

    [Feature] Add shadcn and button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 
+import { LucidePenTool } from "lucide-react";
+
 import Logo from "@/../public/logo.png";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
@@ -15,12 +18,18 @@ export default function Home() {
 
       <h1 className="text-4xl font-bold text-center">
         Study
-        <span className="text-yellow-500">Forge</span>
+        <span className="text-primary">Forge</span>
       </h1>
 
-      <Link href="/auth" className="text-blue-500 underline mt-8">
-        Get started
-      </Link>
+      <Button
+        className="mt-8 w-1/2 max-w-80 animate-pulse focus-within:animate-none focus-within:scale-110 hover:animate-none hover:scale-110 transition-all font-bold"
+        asChild
+      >
+        <Link href="/auth">
+          Get started
+          <LucidePenTool className="-scale-100 !size-6" />
+        </Link>
+      </Button>
     </div>
   );
 }
