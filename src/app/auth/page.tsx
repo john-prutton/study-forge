95e8e67fd13e287f3511cab58f018819e841af5f
John P <johnrprutton@gmail.com>
2025-01-30 22:22:45 +0200

    [Refactor] Initial clean-up

---
+++ src/app/auth/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div className="h-screen grid place-items-center">
+      <div>
+        <h1 className="text-4xl font-bold text-white">Login here</h1>
+      </div>
+    </div>
+  );
+}
