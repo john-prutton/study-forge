78cc956a5e43042f9b18a174d1451b9bbda9008f
John P <johnrprutton@gmail.com>
2025-01-31 20:34:08 +0200

    [Feature] Add skeleton auth form

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,9 +1,9 @@
-export default function LoginPage() {
+import { AuthForm } from "@/components/auth-form";
+
+export default function AuthPage() {
   return (
-    <div className="h-screen grid place-items-center">
-      <div>
-        <h1 className="text-4xl font-bold text-white">Login here</h1>
-      </div>
+    <div className="flex min-h-screen items-center justify-center">
+      <AuthForm />
     </div>
   );
 }
